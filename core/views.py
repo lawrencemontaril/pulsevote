7171bc2d905ed394e8b232ff9f61a914fec4c39f
onlydhru <codesmlbb@gmail.com>
2025-11-19 16:23:45 +0800

    easter: andrew was here

--- core/views.py
+++ core/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def andrew(request):
+    return render(request, 'pages/home.html', {
+        'message': 'andrew was here'
+    })
